b70e071c0c59479360ace6e9e7c807c289e47574
Aliaksei Strakhau <straxov.9942@gmail.com>
2025-01-26 18:07:54 +0300

    HW20\Add Allure Reports

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from app.task_manager import TaskManager
 
 
@@ -10,89 +11,143 @@ def task_manager():
     return TaskManager()
 
 
-def test_add_task_and_set_priority(task_manager):
+@pytest.fixture
+def task_manager_with_tasks():
     """
-    Тестирует функцию добавления задачи в Task Manager c выставлением приоритета
+    Создает экземпляр класса TaskManager c задачами
     """
-    added_task = task_manager.add_task(name="Task_1", priority="low")
-    assert added_task["name"] == "Task_1"
-    assert added_task["priority"] == "low"
-    assert added_task["completed"] is False
-    assert len(task_manager.tasks) == 1
+    task_manager = TaskManager()
+    task_manager.add_task(name="Task_1")
+    task_manager.add_task(name="Task_2", priority="high")
+    return task_manager
 
 
-def test_add_task_without_priority(task_manager):
+@allure.feature("Добавление задачи в Task Manager")
+@allure.story("Успешное добавление задачи")
+@allure.severity(allure.severity_level.BLOCKER)
+@pytest.mark.parametrize(
+    "name, priority, expected",
+    [
+        ("Task_1", "low", [{"completed": False,
+                            "name": "Task_1",
+                            "priority": "low"
+                            }]),
+        ("Task_2", "high", [{"completed": False,
+                             "name": "Task_2",
+                             "priority": "high"
+                             }]),
+        ("Task_3", None, [{"completed": False,
+                           "name": "Task_3",
+                           "priority": "normal"
+                           }])
+    ]
+)
+def test_add_task(task_manager, name, priority, expected):
     """
-    Тестирует функцию добавления задачи в Task Manager без указания приоритета
+    Тестирует функцию добавления задачи в Task Manager
     """
-    added_task = task_manager.add_task(name="Task_2")
-    assert added_task["name"] == "Task_2"
-    assert added_task["priority"] == "normal"
-    assert added_task["completed"] is False
-    assert len(task_manager.tasks) == 1
+
+    if priority is None:
+        with allure.step("Добавить задачу без указания приоритета"):
+            task_manager.add_task(name)
+    else:
+        with allure.step("Добавить задачу c указанием приоритета"):
+            task_manager.add_task(name, priority)
+    with allure.step("Проверить, что задача была добавлена"):
+        assert task_manager.tasks == expected
 
 
-def test_add_task_with_incorrect_priority(task_manager):
+@allure.feature("Добавление задачи в Task Manager")
+@allure.story("Добавление задачи с указанием некорректного приоритета")
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.parametrize(
+    "name, priority",
+    [
+        ("Task_1", "TEST"),
+        ("Task_2", 123),
+        ("Task_4", "lowest")
+    ]
+)
+def test_add_task_with_incorrect_priority(task_manager, name, priority):
     """
     Тестирует функцию добавления задачи в Task Manager
     с указанием приоритета не из списка
     """
-    with pytest.raises(ValueError):
-        task_manager.add_task(name="Task_3", priority="TEST")
+    with allure.step("Добавить задачу с некорректным приоритетом"):
+        with pytest.raises(ValueError):
+            task_manager.add_task(name, priority)
 
 
-def test_list_tasks(task_manager):
+@allure.feature("Отображение списка всех задач в Task Manager")
+@allure.story("Успешное отображение списка всех задач")
+@allure.severity(allure.severity_level.CRITICAL)
+def test_list_tasks(task_manager_with_tasks):
     """
     Тестирует возвращение списка всех задач
     """
-    task_manager.add_task(name="Task_1")
-    task_manager.add_task(name="Task_2", priority="high")
-    assert isinstance(task_manager.list_tasks(), list)
-    assert len(task_manager.list_tasks()) == 2
-    assert task_manager.list_tasks() == [{"completed": False,
-                                          "name": "Task_1",
-                                          "priority": "normal"
-                                          },
-                                         {"completed": False,
-                                          "name": "Task_2",
-                                          "priority": "high"
-                                          }
-                                         ]
+    with allure.step("Проверить отображение всех задач"):
+        assert task_manager_with_tasks.list_tasks() == [{"completed": False,
+                                                         "name": "Task_1",
+                                                         "priority": "normal"
+                                                         },
+                                                        {"completed": False,
+                                                         "name": "Task_2",
+                                                         "priority": "high"
+                                                         }
+                                                        ]
 
 
-def test_mark_task_completed(task_manager):
+@allure.feature('Перевод задачи в статус "completed" в Task Manager')
+@allure.story('Успешный перевод статуса задачи в "completed"')
+@allure.severity(allure.severity_level.CRITICAL)
+def test_mark_task_completed(task_manager_with_tasks):
     """
     Тестирует функцию перевода статуса задачи в выполненную
     """
-    task_manager.add_task(name="task to complete", priority="high")
-    completed_task = task_manager.mark_task_completed("task to complete")
-    assert completed_task["completed"] is True
+    with allure.step('Перевести статус задачи в "completed"'):
+        completed_task = task_manager_with_tasks.mark_task_completed("Task_1")
+    with allure.step('Проверить, что задача изменила статус на "completed"'):
+        assert completed_task["completed"] is True
 
 
-def test_mark_task_completed_not_found(task_manager):
+@allure.feature('Перевод задачи в статус "completed" в Task Manager')
+@allure.story('Перевод несуществующей задачи в статус "completed"')
+@allure.severity(allure.severity_level.NORMAL)
+def test_mark_task_completed_not_found(task_manager_with_tasks):
     """
     Тестирует случай появления исключения,
     когда задача для перевода в выполненную не найдена
     """
-    task_manager.add_task(name="task to complete", priority="high")
-    with pytest.raises(ValueError):
-        task_manager.mark_task_completed("non-existing task")
+    with allure.step("Проверить появление ошибки " +
+                     "при попытке изменения статуса для несуществующей задачи"
+                     ):
+        with pytest.raises(ValueError):
+            task_manager_with_tasks.mark_task_completed("non-existing task")
 
 
-def test_remove_task(task_manager):
+@allure.feature("Удаление задачи из Task Manager")
+@allure.story("Успешное удаление задачи")
+@allure.severity(allure.severity_level.CRITICAL)
+def test_remove_task(task_manager_with_tasks):
     """
     Тестирует функцию удаления задачи
     """
-    task_manager.add_task(name="task to remove", priority="low")
-    task_manager.remove_task("task to remove")
-    assert len(task_manager.tasks) == 0
+    with allure.step("Удалить задачу"):
+        task_manager_with_tasks.remove_task("Task_2")
+    with allure.step("Проверить, что задача была удалена"):
+        assert any("Task_2" not in task["name"] for task in task_manager_with_tasks.tasks)
 
 
-def test_remove_task_not_found(task_manager):
+@allure.feature("Удаление задачи из Task Manager")
+@allure.story("Удаление несуществующей задачи")
+@allure.severity(allure.severity_level.NORMAL)
+def test_remove_task_not_found(task_manager_with_tasks):
     """
     Тестирует случай появления исключения,
     когда задача для удаления не была найдена
     """
-    task_manager.add_task(name="task to remove", priority="low")
-    with pytest.raises(ValueError):
-        task_manager.remove_task("non-existing task")
+    with allure.step("Проверить появление ошибки " +
+                     "при попытке удаления несуществующей задачи"
+                     ):
+        with pytest.raises(ValueError):
+            task_manager_with_tasks.remove_task("non-existing task")
